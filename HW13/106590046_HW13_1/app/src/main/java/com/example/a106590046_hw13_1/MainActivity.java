dcfb629860029a2b342b5971fccea923d486ed09
Melziiie <48340014+Melziiie@users.noreply.github.com>
2021-06-14 12:49:20 +0800

    HW13_1

---
+++ HW13/106590046_HW13_1/app/src/main/java/com/example/a106590046_hw13_1/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.a106590046_hw13_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    public ImageView imgBattery;
+    public int count=0;
+//    public LevelListDrawable levelList = new LevelListDrawable();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imgBattery = (ImageView) findViewById(R.id.imageView2);
+    }
+
+    public void levelDown(View view) {
+        if (count > 0) count--;
+        imgBattery.setImageLevel(count);
+    }
+
+    public void levelUp(View view) {
+        if (count < 5) count++;
+        imgBattery.setImageLevel(count);
+    }
+}

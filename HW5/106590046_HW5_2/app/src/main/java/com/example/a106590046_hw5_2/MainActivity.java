4c0c8b7c8ad4bf5223e7cba7808d4a1a0bbff648
Melziiie <48340014+Melziiie@users.noreply.github.com>
2021-06-15 17:11:25 +0800

    do.it

--- HW5/106590046_HW5_2/app/src/main/java/com/example/a106590046_hw5_2/MainActivity.java
+++ HW5/106590046_HW5_2/app/src/main/java/com/example/a106590046_hw5_2/MainActivity.java
@@ -7,7 +7,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.;
 
 import android.view.View;
 
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
